4e8266787f93a379baac79b378c30194e8ceeccd
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-05-04 20:00:25 -0300

    DEPT_LOCATIONS, PROJECT, WORKS_ON and DEPENDENT tables created.

--- Database/database_script.sql
+++ Database/database_script.sql
@@ -3,7 +3,7 @@ Name:         Roger Silva Santos Aguiar
 Function:     Script implementation of the COMPANY database of the book Database Systems Sixth Edition. 
               Ramez Elmasri and Shamkant B. Navathe
 Initial date: May 2, 2020 
-Last update:  May 2, 2020 
+Last update:  May 4, 2020 
 */
 
 -- It creates a table named EMPLOYEE
@@ -22,7 +22,7 @@ CREATE TABLE EMPLOYEE
     PRIMARY KEY (Ssn)    
 );
 
--- The following script adds a new tuple to the DEPARTMENT relation
+-- The following script creates a new table named DEPARTMENT
 CREATE TABLE DEPARTMENT
 (
     Dname          VARCHAR(15) NOT NULL,
@@ -32,6 +32,50 @@ CREATE TABLE DEPARTMENT
     PRIMARY KEY (Dnumber),
     UNIQUE (Dname),
     FOREIGN KEY (Mgr_ssn) REFERENCES EMPLOYEE(Ssn)
+);
+
+-- The following script creates a new table named DEPT_LOCATIONS
+CREATE TABLE DEPT_LOCATIONS
+(
+    Dnumber   INT         NOT NULL,
+    Dlocation VARCHAR(15) NOT NULL,
+    PRIMARY KEY (Dnumber, Dlocation),
+    FOREIGN key (Dnumber) REFERENCES DEPARTMENT (Dnumber)
+);
+
+-- The following script creates a new table named PROJECT
+CREATE TABLE PROJECT
+(
+    Pname     VARCHAR(15) NOT NULL,
+    Pnumber   INT         NOT NULL,
+    Plocation VARCHAR(15),
+    Dnum      INT         NOT NULL,
+    PRIMARY KEY(Pnumber),
+    UNIQUE(Pname),
+    FOREIGN KEY(Dnum) REFERENCES DEPARTMENT(Dnumber)
+);
+
+-- The following script creates a new table named WORKS_ON
+CREATE TABLE WORKS_ON
+(
+    Essn  CHAR(9)      NOT NULL,
+    Pno   INT          NOT NULL,
+    Hours DECIMAL(3,1) NOT NULL,
+    PRIMARY KEY(Essn, Pno),
+    FOREIGN KEY(Essn) REFERENCES EMPLOYEE(Ssn),
+    FOREIGN KEY(Pno)  REFERENCES PROJECT(Pnumber)
+);
+
+-- The following script creates a new table named DEPENDENT
+CREATE TABLE DEPENDENT
+(
+    Essn           CHAR(9)     NOT NULL,
+    Dependent_name VARCHAR(15) NOT NULL,
+    Sex            CHAR,
+    Bdate          DATE,
+    Relationship   VARCHAR(8),
+    PRIMARY KEY(Essn, Dependent_name),
+    FOREIGN KEY(Essn) REFERENCES EMPLOYEE(Ssn)
 );
 
 -- **********************************************************************************************
@@ -55,3 +99,59 @@ INSERT INTO DEPARTMENT
 VALUES ('Research', 5, '333445555', '1988-05-22'),
        ('Administration', 4, '987654321', '1995-01-01'),
        ('Headquarters', 1, '888665555', '1981-06-19');
+
+-- The following script adds a new tuple to the DEPT_LOCATIONS relation
+INSERT INTO DEPT_LOCATIONS
+VALUES (1, "Houston"),
+       (4, "Stafford"),
+       (5, "Bellaire"),
+       (5, "Sugarland"),
+       (5, "Houston");
+
+-- The following script adds a new tuple to the PROJECT relation
+INSERT INTO PROJECT
+VALUES ("ProductX", 1, "Bellaire", 5),
+       ("ProductY", 2, "Sugarland", 5),
+       ("ProductZ", 3, "Houston", 5),
+       ("Computerization", 10, "Stafford", 4),
+       ("Reorganization", 20, "Houston", 1),
+       ("Newbenefits", 30, "Starfford", 4);
+
+-- The following script adds a new tuple to the WORKS_ON relation
+INSERT INTO WORKS_ON 
+VALUES ("123456789", 1, 32.5),
+       ("123456789", 2, 7.5),
+       ("666884444", 3, 40.0),
+       ("453453453", 1, 20.0),
+       ("453453453", 2, 20.0),
+       ("333445555", 2, 10.0),
+       ("333445555", 3, 10.0),
+       ("333445555", 10, 10.0),
+       ("333445555", 20, 10.0),
+       ("999887777", 30, 30.0),
+       ("999887777", 10, 10.0),
+       ("987987987", 10, 35.0),
+       ("987987987", 30, 5.0),
+       ("987654321", 30, 20.0),
+       ("987654321", 20, 15.0),
+       ("888665555", 20, 0);
+
+-- The following script adds a new tuple to the DEPENDENT relation
+INSERT INTO DEPENDENT
+VALUES ("333445555", "Alice", "F", "1986-04-05", "Daughter"),
+       ("333445555", "Theodore", "M", "1983-10-25", "Son"),
+       ("333445555", "Joy", "F", "1958-05-03", "Spouse"),
+       ("987654321", "Abner", "M", "1942-02-28", "Spouse"),
+       ("123456789", "Michael", "M", "1988-01-04", "Son"),
+       ("123456789", "Alice", "F", "1988-12-30", "Daughter"),
+       ("123456789", "Elizabeth", "F", "1967-05-05", "Spouse");
+
+-- *********************************************************************************************************
+-- Basic queries to test the tables
+
+SELECT * FROM EMPLOYEE;
+SELECT * FROM DEPARTMENT;
+SELECT * FROM DEPT_LOCATIONS;
+SELECT * FROM PROJECT;
+SELECT * FROM WORKS_ON;
+SELECT * FROM DEPENDENT;

184e237579d9c96570c336452d8741b718157a05
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-05-10 19:30:28 -0300

    Updated to PostgreeSql sintaxe.

--- Database/database_script.sql
+++ Database/database_script.sql
@@ -3,7 +3,7 @@ Name:         Roger Silva Santos Aguiar
 Function:     Script implementation of the COMPANY database of the book Database Systems Sixth Edition. 
               Ramez Elmasri and Shamkant B. Navathe
 Initial date: May 2, 2020 
-Last update:  May 4, 2020 
+Last update:  May 10, 2020 
 */
 
 CREATE DATABASE COMPANY;-- It creates a new database
@@ -105,49 +105,49 @@ VALUES ('Research', 5, '333445555', '1988-05-22'),
 
 -- The following script adds a new tuple to the DEPT_LOCATIONS relation
 INSERT INTO DEPT_LOCATIONS
-VALUES (1, "Houston"),
-       (4, "Stafford"),
-       (5, "Bellaire"),
-       (5, "Sugarland"),
-       (5, "Houston");
+VALUES (1, 'Houston'),
+       (4, 'Stafford'),
+       (5, 'Bellaire'),
+       (5, 'Sugarland'),
+       (5, 'Houston');
 
 -- The following script adds a new tuple to the PROJECT relation
 INSERT INTO PROJECT
-VALUES ("ProductX", 1, "Bellaire", 5),
-       ("ProductY", 2, "Sugarland", 5),
-       ("ProductZ", 3, "Houston", 5),
-       ("Computerization", 10, "Stafford", 4),
-       ("Reorganization", 20, "Houston", 1),
-       ("Newbenefits", 30, "Starfford", 4);
+VALUES ('ProductX', 1,' Bellaire', 5),
+       ('ProductY', 2, 'Sugarland', 5),
+       ('ProductZ', 3, 'Houston', 5),
+       ('Computerization', 10, 'Stafford', 4),
+       ('Reorganization', 20, 'Houston', 1),
+       ('Newbenefits', 30, 'Starfford', 4);
 
 -- The following script adds a new tuple to the WORKS_ON relation
 INSERT INTO WORKS_ON 
-VALUES ("123456789", 1, 32.5),
-       ("123456789", 2, 7.5),
-       ("666884444", 3, 40.0),
-       ("453453453", 1, 20.0),
-       ("453453453", 2, 20.0),
-       ("333445555", 2, 10.0),
-       ("333445555", 3, 10.0),
-       ("333445555", 10, 10.0),
-       ("333445555", 20, 10.0),
-       ("999887777", 30, 30.0),
-       ("999887777", 10, 10.0),
-       ("987987987", 10, 35.0),
-       ("987987987", 30, 5.0),
-       ("987654321", 30, 20.0),
-       ("987654321", 20, 15.0),
-       ("888665555", 20, 0);
+VALUES ('123456789', 1, 32.5),
+       ('123456789', 2, 7.5),
+       ('666884444', 3, 40.0),
+       ('453453453', 1, 20.0),
+       ('453453453', 2, 20.0),
+       ('333445555', 2, 10.0),
+       ('333445555', 3, 10.0),
+       ('333445555', 10, 10.0),
+       ('333445555', 20, 10.0),
+       ('999887777', 30, 30.0),
+       ('999887777', 10, 10.0),
+       ('987987987', 10, 35.0),
+       ('987987987', 30, 5.0),
+       ('987654321', 30, 20.0),
+       ('987654321', 20, 15.0),
+       ('888665555', 20, 0);
 
 -- The following script adds a new tuple to the DEPENDENT relation
 INSERT INTO DEPENDENT
-VALUES ("333445555", "Alice", "F", "1986-04-05", "Daughter"),
-       ("333445555", "Theodore", "M", "1983-10-25", "Son"),
-       ("333445555", "Joy", "F", "1958-05-03", "Spouse"),
-       ("987654321", "Abner", "M", "1942-02-28", "Spouse"),
-       ("123456789", "Michael", "M", "1988-01-04", "Son"),
-       ("123456789", "Alice", "F", "1988-12-30", "Daughter"),
-       ("123456789", "Elizabeth", "F", "1967-05-05", "Spouse");
+VALUES ('333445555', 'Alice', 'F', '1986-04-05', 'Daughter'),
+       ('333445555', 'Theodore', 'M', '1983-10-25', 'Son'),
+       ('333445555', 'Joy', 'F', '1958-05-03', 'Spouse'),
+       ('987654321', 'Abner', 'M', '1942-02-28', 'Spouse'),
+       ('123456789', 'Michael', 'M', '1988-01-04', 'Son'),
+       ('123456789', 'Alice', 'F', '1988-12-30', 'Daughter'),
+       ('123456789', 'Elizabeth', 'F', '1967-05-05', 'Spouse');
 
 -- *********************************************************************************************************
 -- Basic queries to test the tables

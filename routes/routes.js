ceb4df2d3e409bfef74d70909b61de8426584142
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-05-16 13:00:10 -0300

    update route for EMPLOYEE table created.

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,8 @@
 /* 
 Name:         Roger Silva Santos Aguiar
-Function:     This file contains all the routes of the system
+Function:     This file contains the routes for all the system.
 Initial date: May 5, 2020
-Last update:  May 14, 2020 
+Last update:  May 16, 2020 
 */
 
 var express = require('express');
@@ -19,10 +19,12 @@ router.get
     } 
 )
 
+/*========================================EMPLOYEE routes======================================== */
+
 //This route shows all the rows of the EMPLOYEE table
 router.get
 (
-    '/employee', 
+    '/show_employees', 
     function (req, res)
     {
         myModules.show(res);//It shows all the rows or the EMPLOYEE table        
@@ -54,6 +56,29 @@ router.get
 
 router.get
 (
+    '/update/:fname/:minit/:lname/:ssn/:bdate/:address/:sex/:salary/:super_ssn/:dno', 
+    function (req, res)
+    {
+        //Variables
+        //It gets the params
+        var fname = req.params.fname;
+        var minit = req.params.minit;
+        var lname = req.params.lname;
+        var ssn = req.params.ssn;
+        var bdate = req.params.bdate;
+        var address = req.params.address;
+        var sex = req.params.sex;
+        var salary = req.params.salary;
+        var super_ssn = req.params.super_ssn;
+        var dno = req.params.dno;
+
+        //It inserts a new row to the EMPLOYEE table using the following module
+        myModules.update(res, fname, minit, lname, ssn, bdate, address, sex, salary, super_ssn, dno);                
+    } 
+);//End add route
+
+router.get
+(
     '/delete/:ssn', 
     function (req, res)
     {
@@ -65,6 +90,10 @@ router.get
         myModules.delete_row(res, ssn);                
     } 
 );//End delete route
+
+/*=======================================End EMPLOYEE routes======================================= */
+
+/*========================================DEPARTMENT routes======================================== */
 
 //It exports the module, it becomes it available to other external files
 module.exports = router;

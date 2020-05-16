4a02d012e5648cd57a97a73cef4454af644f850b
Roger Aguiar <roger.silvaaguiar@yahoo.com.br>
2020-05-16 13:00:56 -0300

    No update.

--- server.js
+++ server.js
@@ -2,13 +2,13 @@
 Name:         Roger Silva Santos Aguiar
 Function:     Server for the application 
 Initial date: May 5, 2020
-Last update:  May 13, 2020 
+Last update:  May 16, 2020 
 */
 const express = require('express');
 const app = express();
 const port = 5000
 
-//It uses this file to access the routes
+//It uses this file to access the routes that refers to the EMPLOYEE table
 app.use('/', require('./routes/routes'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
